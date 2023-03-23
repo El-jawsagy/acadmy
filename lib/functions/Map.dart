f59f89814322590ec461ac8170eb3e103a16b530
Hussam208 <Hussamgamal2@gmail.com>
2023-03-23 13:37:09 +0200

    habebe elmap shaghala zy elamar zayak

--- lib/functions/Map.dart
+++ lib/functions/Map.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:location/location.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapScreen extends StatefulWidget {
   @override
@@ -9,65 +9,77 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  LocationData? _locationData;
+  final MapController _mapController = MapController();
 
-  @override
-  void initState() {
-    super.initState();
-    _getCurrentLocation();
+  void _onZoomInPressed() {
+    _mapController.move(_mapController.center, _mapController.zoom + 1);
   }
 
-  Future<void> _getCurrentLocation() async {
-    final location = Location();
-    try {
-      final currentLocation = await location.getLocation();
-      setState(() {
-        _locationData = currentLocation;
-      });
-    } catch (e) {
-      print('Error: $e');
-    }
+  void _onZoomOutPressed() {
+    _mapController.move(_mapController.center, _mapController.zoom - 1);
   }
+
+  void _onCurrentLocationPressed() async {
+    // Get the current position
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    // Move the map to the current position
+    _mapController.move(LatLng(position.latitude, position.longitude), 15.0);
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('OpenStreetMap'),
-      ),
       body: FlutterMap(
+        mapController: _mapController,
         options: MapOptions(
-          center: _locationData != null
-              ? LatLng(_locationData!.latitude!, _locationData!.longitude!)
-              : LatLng(51.5, -0.09),
-          zoom: 13.0,
+          center: LatLng(30.0561,31.2394), // default center of the map
+          zoom: 13.0, // default zoom level
         ),
         layers: [
           TileLayerOptions(
-            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
             subdomains: ['a', 'b', 'c'],
           ),
           MarkerLayerOptions(
-            markers: _locationData != null
-                ? [
+            markers: [
               Marker(
                 width: 80.0,
                 height: 80.0,
-                point:
-                LatLng(_locationData!.latitude!, _locationData!.longitude!),
+                point: LatLng(30.0561, 31.2394),
                 builder: (ctx) => Container(
-                  child: Icon(
-                    Icons.location_pin,
-                    color: Colors.red,
-                    size: 50.0,
-                  ),
+                  child: SizedBox(),
                 ),
-              )
-            ]
-                : [],
+              ),
+            ],
+          ),
+        ],
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: null,
+            onPressed: _onZoomInPressed,
+            child: Icon(Icons.add),
+          ),
+          SizedBox(height: 10),
+          FloatingActionButton(
+            heroTag: null,
+            onPressed: _onZoomOutPressed,
+            child: Icon(Icons.remove),
+          ),
+          SizedBox(height: 10),
+          FloatingActionButton(
+            heroTag: null,
+            onPressed: _onCurrentLocationPressed,
+            child: Icon(Icons.my_location),
           ),
         ],
       ),
     );
   }
 }
+
